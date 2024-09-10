d1550dc3187c7b4e44921ae90ab2db027cc6a542
Jordan Youssef <jordanyoussef28@gmail.com>
2024-09-09 20:45:51 -0500

    profiule api calls and sample data files

---
+++ api/profileData.js
@@ -0,0 +1,33 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getProfile = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const createProfile = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getProfile,
+  createProfile,
+};
