5f8fcd43faf7ceaadbd861790d61740e1c3872e8
Jordan Youssef <jordanyoussef28@gmail.com>
2024-09-13 12:59:22 -0500

    create project form and project api calls

---
+++ pages/projects/new.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProjectForm from '../../components/forms/ProjectForm';
+
+// reusable form to add/edit project and render in this view
+
+export default function AddProject() {
+  return <ProjectForm />;
+}
