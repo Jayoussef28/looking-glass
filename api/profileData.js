ee0c8843bbf2f92c0e8ef8e80cad39835358ebb4
Jordan Youssef <jordanyoussef28@gmail.com>
2024-09-14 07:48:54 -0500

    user specific profile data working!

--- api/profileData.js
+++ api/profileData.js
@@ -2,8 +2,8 @@ import { clientCredentials } from '../utils/client';
 
 const endpoint = clientCredentials.databaseURL;
 
-const getProfile = () => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/profile.json`, {
+const getProfile = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
